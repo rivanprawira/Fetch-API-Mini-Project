132738db18d30d4015ea414dc106acf9594870e7
rivanprawira <prawirarivan410@gmail.com>
2025-11-19 09:18:01 +0700

    update dasboardPage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { getComments } from "./fetchData";
 import ClientInteractive from "./components/ClientInteractive";
 
 export default async function DashboardPage() {
-  const comments = await getComments(); // SSR
+  const comments = await getComments();
 
   return (
     <main className="dashboard-page">
